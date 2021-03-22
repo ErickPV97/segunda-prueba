9290362bfb5af449ee2ccd231389143835a15725
ErikPV97 <ErickPV977>
2021-03-22 13:34:37 -0600

    agregue el atentamente

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main()
 {
     printf("Hello world, remember to drink water!\n");
-    printf("/n and goodbye!");
+    printf("\n and goodbye!");
+    printf("\n atte: Erick");
     return 0;
 }
